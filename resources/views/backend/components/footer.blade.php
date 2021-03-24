810665b089b4103c31c16aa6a39906d8bcaa211a
Your name here <your_email@example.com>
2021-03-24 16:56:35 +0600

    design made

---
+++ resources/views/backend/components/footer.blade.php
@@ -0,0 +1,25 @@
+
+  <!-- Remove the container if you want to extend the Footer to full width. -->
+{{-- <div class="container my-5 fixed-bottom" > --}}
+
+    <!-- Footer -->
+    <footer
+            class="text-center text-lg-start"
+            style="background-color: #e41b46"
+            >
+
+
+      <!-- Copyright -->
+      <div
+           class="text-center p-3 fixed-bottom"
+           style="background-color: rgba(255, 0, 0, 0.2)"
+           >
+        © 2021 All Rights Reserved By Frank Stone.
+
+      </div>
+      <!-- Copyright -->
+    </footer>
+    <!-- Footer -->
+
+  </div>
+  <!-- End of .container -->
--- resources/views/backend/components/sidebar.blade.php
+++ resources/views/backend/components/sidebar.blade.php
@@ -1,79 +1,80 @@
-<nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-dark  sidebar collapse">
-      <div class="position-sticky pt-3">
+<nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-info  sidebar collapse">
+    <div class="position-sticky pt-3">
         <ul class="nav flex-column ">
-          <li class="nav-item">
-            <a class="nav-link active " aria-current="page" href="{{'/'}}">
-              <span data-feather="home"></span>
-              Dashboard
-            </a>
-          </li>
+            <li class="nav-item ">
+                <a class="nav-link active text-white" aria-current="page" href="{{ '/' }}">
+                    <span data-feather="home"></span>
+                    Dashboard
+                </a>
+            </li>
 
-          <li class="nav-item">
-            <a class="nav-link text-dark bg-light" href="{{route('emp.dashboard')}}">
-              <span data-feather="home"></span>
-              Employee
-            </a>
-          </li>
+            <li class="nav-item ">
+                <a class="nav-link text-white" href="{{ route('emp.dashboard') }}">
+                    <span data-feather="home"></span>
+                    Employee
+                </a>
+            </li>
 
-          <li class="nav-item">
-            <a class="nav-link text-light" href="{{route('ws.dashboard')}}">
-              <span data-feather="home"></span>
-              Workstation
-            </a>
-          </li>
+            <li class="nav-item">
+                <a class="nav-link text-white" href="{{ route('ws.dashboard') }}">
+                    <span data-feather="home"></span>
+                    Workstation
+                </a>
+            </li>
 
-          <li class="nav-item">
-            <a class="nav-link text-dark bg-light" href="{{route('pl.dashboard')}}">
-              <span data-feather="home"></span>
-              Product Listing
-            </a>
-          </li>
+            <li class="nav-item">
+                <a class="nav-link  text-white" href="{{ route('pl.dashboard') }}">
+                    <span data-feather="home"></span>
+                    Product Listing
+                </a>
+            </li>
 
-          <li class="nav-item">
-            <a class="nav-link text-light" aria-current="page" href="{{route('pd.dashboard')}}">
-              <span data-feather="home"></span>
-              Production Demand
-            </a>
-          </li>
+            <li class="nav-item">
+                <a class="nav-link text-white" aria-current="page" href="{{ route('pd.dashboard') }}">
+                    <span data-feather="home"></span>
+                    Production Demand
+                </a>
+            </li>
 
-          <li class="nav-item">
-            <a class="nav-link text-dark bg-light" aria-current="page" href="{{route('mpp.dashboard')}}">
-              <span data-feather="home"></span>
-              My Production Plan
-            </a>
-          </li>
+            <li class="nav-item">
+                <a class="nav-link  text-white" aria-current="page" href="{{ route('mpp.dashboard') }}">
+                    <span data-feather="home"></span>
+                    My Production Plan
+                </a>
+            </li>
 
-          <li class="nav-item">
-            <a class="nav-link text-light" aria-current="page" href="{{route('pp.dashboard')}}">
-              <span data-feather="home"></span>
-              Production Planning
-            </a>
-          </li>
+            <li class="nav-item">
+                <a class="nav-link text-white" aria-current="page" href="{{ route('pp.dashboard') }}">
+                    <span data-feather="home"></span>
+                    Production Planning
+                </a>
+            </li>
 
-          <li class="nav-item">
-            <a class="nav-link text-dark bg-light" aria-current="page" href="{{route('sto.dashboard')}}">
-              <span data-feather="home"></span>
-              Stock
-            </a>
-          </li>
+            <li class="nav-item">
+                <a class="nav-link  text-white" aria-current="page" href="{{ route('sto.dashboard') }}">
+                    <span data-feather="home"></span>
+                    Stock
+                </a>
+            </li>
 
-          <li class="nav-item">
-            <a class="nav-link text-light" href="{{route('pro.dashboard')}}">
-              <span data-feather="file" ></span>
-              Procurement
-            </a>
-          </li>
-
-
-
-          <li class="nav-item">
-            <a class="nav-link text-dark bg-light" href="{{route('rep.dashboard')}}">
-              <span data-feather="bar-chart-2"></span>
-              Reports
-            </a>
-          </li>
+            <li class="nav-item">
+                <a class="nav-link text-white" href="{{ route('pro.dashboard') }}">
+                    <span data-feather="file"></span>
+                    Procurement
+                </a>
+            </li>
 
 
+
+            <li class="nav-item">
+                <a class="nav-link  text-white" href="{{ route('rep.dashboard') }}">
+                    <span data-feather="bar-chart-2"></span>
+                    Reports
+                </a>
+            </li>
         </ul>
-      </div>
-    </nav>
+    </div>
+</nav>
+
+
+
