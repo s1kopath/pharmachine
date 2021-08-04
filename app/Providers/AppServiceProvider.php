a125142d279d1611e70d966da07ad953c5f4ff60
Your name here <your_email@example.com>
2021-08-05 05:41:13 +0600

    appserviceprovide updated

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -31,7 +31,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-
         Paginator::useBootstrap();
 
         //worker panel
@@ -39,40 +38,35 @@ class AppServiceProvider extends ServiceProvider
             if (Auth::check()) {
                 $view->with('dashHome', Auth::user());
                 if (auth()->user()->role == 'worker') {
-
                     $countRequest = Manufacturing::where('worker_id', auth()->user()->userProfile->id)
-                    ->whereIn('status',['Waiting for production', 'In production'])->count();
-                    $countMachineRepair = WorkstationRepair::where('worker_id',auth()->user()->userProfile->id)->count();
-                    $countMaterialOrder = Material::where('status','Ordered')->count();
+                    ->whereIn('status', ['Waiting for production', 'In production'])->count();
+                    $countMachineRepair = WorkstationRepair::where('worker_id', auth()->user()->userProfile->id)->count();
+                    $countMaterialOrder = Material::where('status', 'Ordered')->count();
                     $countDelivery = Manufacturing::where('worker_id', auth()->user()->userProfile->id)
-                    ->where('status','Ready for shipment')->count();
+                    ->where('status', 'Ready for shipment')->count();
 
                     View()->share('countRequest', $countRequest);
                     View()->share('countMachineRepair', $countMachineRepair);
                     View()->share('countMaterialOrder', $countMaterialOrder);
                     View()->share('countDelivery', $countDelivery);
+
+                    // admin panel
+                    $countDemand = Demand::where('status', 'pending')->count();
+                    $countOrder = Manufacturing::where('status', '!=', 'Delivered')->count();
+                    $countWorkstation = Workstation::whereIn('status', ['Waiting for repair','Workstation damaged'])->count();
+                    $countMaterial = Material::where('available_quantity', '<=', 10)->count();
+                    $countReadyShipment = Manufacturing::where('status', 'Ready for shipment')->count();
+
+
+                    View()->share('workstation', $countWorkstation);
+                    view()->share('demand_count', $countDemand);
+                    view()->share('count_order', $countOrder);
+                    view()->share('count_material', $countMaterial);
+                    view()->share('count_shipment', $countReadyShipment);
                 }
             } else {
                 $view->with('dashHome', null);
             }
         });
-
-
-
-        // admin panel
-        $countDemand = Demand::where('status', 'pending')->count();
-        $countOrder = Manufacturing::where('status', '!=', 'Delivered')->count();
-        $countWorkstation = Workstation::whereIn('status', ['Waiting for repair','Workstation damaged'])->count();
-        $countMaterial = Material::where('available_quantity', '<=', 10)->count();
-        $countReadyShipment = Manufacturing::where('status', 'Ready for shipment')->count();
-
-
-
-        View()->share('workstation', $countWorkstation);
-        view()->share('demand_count', $countDemand);
-        view()->share('count_order', $countOrder);
-        view()->share('count_material', $countMaterial);
-        view()->share('count_shipment', $countReadyShipment);
-
     }
 }
